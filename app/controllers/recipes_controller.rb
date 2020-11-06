85cd7c9fbd66a12e8fa0ac5e84d387bc8ab41cee
Justin Hutsell <hutselljustin@gmail.com>
2020-11-06 15:21:13 -0500

    adds search recipes functionality, small style edits, confirm messages for delete ingredients

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,17 @@
 class RecipesController < ApplicationController
+
+    def show
+    end
+
+    def index
+        
+    end
+
+    def get_recipes
+        url = "http://www.recipepuppy.com/api/?i=#{params["args"]}"
+
+        recipes = RestClient.get(url)
+        @response = JSON.parse(recipes)["results"]
+    end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,13 +1,5 @@
 class Recipe < ApplicationRecord
     has_many :users, through: user_recipes
-    has_many :ingredients, through: recipe_ingredients
-
-    def self.get_recipes_from_recipe_puppy(args)
-        url = "http://www.recipepuppy.com/api/?"
-        params = {
-            i: args
-        }
-        response = RestClient.get(url, params)
-        return response
-    end
+    has_many :ingredients, through: recipe_ingredients 
+    
 end
