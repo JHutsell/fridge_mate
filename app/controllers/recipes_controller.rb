54bd5d1e880b65f1a8f3d6329944f00b9e1c74f0
Justin Hutsell <hutselljustin@gmail.com>
2020-11-08 16:06:01 -0500

    adds current_user_ingredients helper method to application controller, uses this method to conditionally render a flash whilst entering ingredients in recipe search

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,13 @@ class RecipesController < ApplicationController
     end
 
     def get_recipes
-        url = "http://www.recipepuppy.com/api/?i=#{params["args"]}"
+        url = "http://www.recipepuppy.com/api/?i=#{params[:args]}"
+        
+        searched_ingredients_array = params[:args].downcase.split(/\W+/)
+        
+        if !(searched_ingredients_array - current_user_ingredients).empty?
+            flash.alert = "You do not currently have all necessary ingredients!"
+        end
 
         recipes = RestClient.get(url)
         @response = JSON.parse(recipes)["results"]
